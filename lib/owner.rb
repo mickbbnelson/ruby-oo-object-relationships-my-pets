1959de9eba665cc94f2afce8078fd2688091801b
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-03-23 18:42:26 -0700

    commit tree 3b6e6aa1c07d00ffbb5be1585320096ea992a810

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,80 @@
+require "pry"
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.count
+    self.all.size
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats 
+    Cat.all.select {|cats| cats.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dogs| dogs.owner == self}
+  end
+
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+
+  def walk_dogs
+    Dog.all.each do |dog|
+      if dog.owner == self
+      dog.mood = "happy"
+      end
+    end
+  end
+
+  def feed_cats
+    Cat.all.each do |cat|
+      if cat.owner == self
+        cat.mood = "happy"
+      end
+    end
+  end 
+
+  def sell_pets
+    Cat.all.each do |cat|
+      cat.mood = "nervous" if cat.owner == self
+    end
+    Cat.all.each do |cat|
+      cat.owner = nil if cat.owner == self
+    end
+    Dog.all.each do |dog|
+      dog.mood = "nervous" if dog.owner == self
+    end
+    Dog.all.each do |dog|
+      dog.owner = nil if dog.owner == self
+    end
+  end
+
+  def list_pets
+      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
